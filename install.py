34316f95fd5b68c522e80bcebde5637f59441ff8
SpamixOfficial <99183771+SpamixOfficial@users.noreply.github.com>
2022-09-06 15:05:58 +0200

    Files Added

---
+++ install.py
@@ -0,0 +1,59 @@
+import os
+cresult = False
+presult = False
+# Module checking!
+
+print("Checking for the modules on your system...")
+## Checking if modules are installed and configuring settings.txt
+try:
+	import colorama
+	print("Module \"colorama\" was found!")
+	cresult = True
+except ModuleNotFoundError:
+	print("The module named \"colorama\" wasn't found! Do you want to install it? (y/n)")
+	coloramainput = input().upper()
+	if coloramainput == "Y":
+		os.system("pip install colorama")
+	elif coloramainput == "N":
+		print("Then you need to install it. Read the instructions on the github page or read the README.md!")
+	else:
+		print("That wasn't a N or an Y.")
+		quit()
+
+try:
+	import pynput
+	print("Module \"pynput\" was found!")
+	presult = True
+except ModuleNotFoundError:
+	print("The module named \"pynput\" wasn't found! Do you want to install it? (y/n)")
+	pynputinput = input().upper()
+	if pynputinput == "Y":
+		os.system("pip install pynput")
+	elif pynputinput == "N":
+		print("Then you need to install it. Read the instructions on the github page or read the README.md!")
+	else:
+		print("That wasn't a N or an Y.")
+		quit()
+
+	
+if presult == True and cresult == True:	
+	f = open('./settings.txt','r')
+	a = ['firststartup=false']
+	lst = []
+	for line in f:
+	    for word in a:
+	        if word in line:
+	            line = line.replace(word,'')
+	    lst.append(line)
+	f.close()
+	f = open('./settings.txt','w')
+	for line in lst:
+	    f.write(line)
+	f.write("firststartup=true")
+	f.close()
+	print("You are ready to go!")
+
+
+else:
+	print(Back.RED + "Sorry, but the either one or both of the modules wasn't found. \n Read the instructions for help on how to install them!")
+	quit()
---
+++ main.py
@@ -0,0 +1,81 @@
+import os, time
+from pynput.keyboard import Key, Listener
+from pynput.mouse import Button, Controller
+from colorama import Fore, Back, init
+
+# Settings checking!
+firststartup = False
+if True:
+	checkstartup = "firststartup=true"
+	checkstartup2 = "firststartup=false"
+	settingsfile = open("settings.txt", 'r+')
+	readfilesize = 'settings.txt'
+	if checkstartup in settingsfile:
+		firststartup = True
+	elif checkstartup2 in settingsfile:
+		firststartup = False
+
+	settingsfile.close()
+
+## --------------------------------------------------------------- ##
+# Start of program
+
+init(autoreset=True)
+if firststartup == False:
+	print("Run the installation script before running the main program!")
+	quit()
+
+mouse = Controller()
+os.system("clear")
+
+
+openlogo = """ 
+  /$$$$$$                                 /$$$$$$  /$$ /$$           /$$      
+ /$$__  $$                               /$$__  $$| $$|__/          | $$      
+| $$  \ $$  /$$$$$$   /$$$$$$  /$$$$$$$ | $$  \__/| $$ /$$  /$$$$$$$| $$   /$$
+| $$  | $$ /$$__  $$ /$$__  $$| $$__  $$| $$      | $$| $$ /$$_____/| $$  /$$/
+| $$  | $$| $$  \ $$| $$$$$$$$| $$  \ $$| $$      | $$| $$| $$      | $$$$$$/ 
+| $$  | $$| $$  | $$| $$_____/| $$  | $$| $$    $$| $$| $$| $$      | $$_  $$ 
+|  $$$$$$/| $$$$$$$/|  $$$$$$$| $$  | $$|  $$$$$$/| $$| $$|  $$$$$$$| $$ \  $$
+ \______/ | $$____/  \_______/|__/  |__/ \______/ |__/|__/ \_______/|__/  \__/
+          | $$                                                                
+          | $$                                                                
+          |__/                                                                
+
+                                                                              
+							SpamixOfficial 2022                                                          
+"""
+
+
+for a in "Hello and welcome to":
+	time.sleep(0.1)
+	print(Fore.RED + a, end="")
+time.sleep(0.6)
+for a in "...":
+	print(Fore.RED + a, end="")
+	time.sleep(1)
+
+os.system("clear")
+for char in openlogo:
+	print(Fore.RED + char, end="")
+	time.sleep(0.003)
+
+## Start of clicker code
+print("Controls: \nF1 to click \nEsc to exit the script!")
+def on_press(key):
+    if key == Key.f1:
+        mouse.press(Button.left)
+        mouse.release(Button.left)
+    if key == Key.delete:
+    	print("work :D")
+
+def on_release(key):
+    if key == Key.esc:
+        # Stop listener
+        return False
+
+# Collect events until released
+with Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
