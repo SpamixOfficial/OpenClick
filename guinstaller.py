1a09b814790bdaea17539c0361f9baf79b5b0587
SpamixOfficial <99183771+SpamixOfficial@users.noreply.github.com>
2023-07-22 18:23:20 +0200

    finished guinstaller.py

--- guinstaller.py
+++ guinstaller.py
@@ -1,11 +1,10 @@
 import subprocess, threading
 
 def install_ctk():
-    global root
+    global root, proc, stdoutdata, window1
     ctk_thread = threading.Thread()
     proc = subprocess.Popen("pip install customtkinter", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     stdoutdata, stderrdata = proc.communicate()
-    print(proc.returncode)
     root.destroy()
     if not proc.returncode == 0:
         window1 = Tk()
@@ -14,9 +13,26 @@ def install_ctk():
         ttk.Label(frm, text="")
         ttk.Label(frm, text='Installation failed').grid(column=0, row=0)
         ttk.Button(frm, text='Quit', command=window1.destroy).grid(column=0, row=1)
+        ttk.Button(frm, text='Show Error Message', command=show_error).grid(column=1, row=1)
+    elif proc.returncode == 0:
+        window1 = Tk()
+        frm = ttk.Frame(window1, padding=10)
+        frm.grid()
+        ttk.Label(frm, text="")
+        ttk.Label(frm, text='Installation was a success!').grid(column=0, row=0)
+        ttk.Button(frm, text='Quit', command=window1.destroy).grid(column=0, row=1)
+def show_error():
+    global proc
+    window1.destroy()
+    window2 = Tk()
+    frm = ttk.Frame(window2, padding=10)
+    frm.grid()
+    ttk.Label(frm, text=stdoutdata).grid(column=0, row=0)
+    ttk.Button(frm, text='Quit', command=window1.destroy).grid(column=0, row=26)
 
 def pip_install_ctk():
     proc = subprocess.Popen("pip install customtkinter", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
 
 try:
     import customtkinter
